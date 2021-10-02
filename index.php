55fa117c9fbe1e9e5a5760290a8bcae13c97297c
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-03 01:06:34 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+	<meta charset="utf-8">
+	<title>Upload de ficheiros</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1 class="title">Upload de Ficheiros</h1>
+
+	<form action="upload.php" method="post" enctype="multipart/form-data" name="cadatro">
+		Titulo do Ficheiro<input type="text" name="ficheiro">
+		<input type="file" name="foto" accept='image/*'>
+		<input type="submit" name="cadastrar" value="Cadastrar">
+		
+	</form>
+
+</body>
+</html>
