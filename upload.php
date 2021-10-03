62131bb248e0e3c17a7009615b41190f517cb528
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-03 02:16:43 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,7 +3,20 @@
 	//chamando a conexao
 	$connection = include_once('./conexao.php');
 
-	var_dump($_POST['cadastrar']);
-	var_dump($_FILES)
+	$foto = $_FILES['foto'];
+	//var_dump($foto);
+
+	if ($foto['name'] != '') {
+		//pegar o nome da foto, mas com o MD5 para evitar repeticao de nomes na base de dados e concatenar com um numero RANDOM
+		$nameImagem = md5($foto['name'] .rand(0,9999));
+
+		//variavel para pegar e armazenar a extensao da imagem
+		$tipo = 
+
+		var_dump($nameImagem);
+	} else {
+		// code...
+	}
+	
 
 ?>
