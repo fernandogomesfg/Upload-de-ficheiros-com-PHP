a6058b16c81effd7ea48fd8fd510dc816cade642
Fernando Gomes <fernandogomesbernardo@gmail.com>
2021-10-04 21:59:39 +0200

    Update exibirFicheiros.php

--- exibirFicheiros.php
+++ exibirFicheiros.php
@@ -4,9 +4,23 @@
 	$sql = 'select * from produto';
 	$result = $connection -> prepare($sql);
 	$result -> execute();
-
-
 	$ficheiros = $result -> fetchAll(PDO::FETCH_OBJ);
+
+
+	//para impressao das imagens na tela
+	$html = "<div>";
+	foreach ($ficheiros as $ficheiro) {
+		$html .= "
+		<div>
+			<h1>{$produto -> nome}</h1>
+			<img src=./img/{$ficheiro->imagem}>
+		</div>
+		";
+	}
+	$html .='</div>';
+
+	return $html;
+
 	
 
 
